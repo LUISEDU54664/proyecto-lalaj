53b0d7748c40e582f29641b2a5211a303102ae22
LUISEDU54664 <111461721+LUISEDU54664@users.noreply.github.com>
2022-09-26 10:13:08 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,23 @@
 input.onButtonPressed(Button.A, function () {
     if (distancia == 0 && distancia < 9) {
         total += total + 50
+        basic.showNumber(50)
     }
     if (distancia == 9 && distancia < 12) {
         total += total + 100
+        basic.showNumber(100)
     }
     if (distancia == 12 && distancia < 15) {
         total += total + 200
+        basic.showNumber(200)
     }
     if (distancia == 15 && distancia < 19) {
         total += total + 500
+        basic.showNumber(500)
     }
     if (distancia == 19 && distancia < 22) {
         total += total + 1000
+        basic.showNumber(1000)
     }
 })
 let total = 0
